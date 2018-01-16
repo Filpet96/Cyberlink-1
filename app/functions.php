5ffb91259c203995d48654fa522544d4c2f17939
Joakim Sjöbäck <joakim.sjoback@hotmail.com>
2018-01-16 15:49:34 +0100

    removed some commented lines

--- app/functions.php
+++ app/functions.php
@@ -3,13 +3,6 @@ declare(strict_types=1);
 
 
 if (!function_exists('redirect')) {
-  // /**
-  // * Redirect the user to given path.
-  // *
-  // * @param string $path
-  // *
-  // * @return void
-  // */
   function redirect($path)
   {
     header("Location: ${path}");
